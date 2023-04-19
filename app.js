1c7634cda5a21eba8a64cc2cf6f6b856f9cf12fe
ShangWei Huang <shangwei.huang@iherb.com>
2023-04-19 20:42:44 +0800

    add flash messages

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 
 const { engine } = require('express-handlebars')
@@ -16,6 +18,13 @@ app.set('views', './views');
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+app.use(session({
+	secret: 'ThisIsSecret',
+	resave: false,
+	saveUninitialized: true
+}))
+app.use(flash())
+
 app.get('/', (req, res) => {
 	res.render('index')
 })
@@ -25,7 +34,7 @@ app.get('/todos', (req, res) => {
 		attributes: ['id', 'name', 'isComplete'],
 		raw: true
 	})
-		.then((todos) => res.render('todos', { todos }))
+		.then((todos) => res.render('todos', { todos, message: req.flash('success') }))
 		.catch((err) => res.status(422).json(err))
 })
 
@@ -37,7 +46,10 @@ app.post('/todos', (req, res) => {
 	const name = req.body.name
 	
 	return Todo.create({ name })
-		.then(() => res.redirect('/todos'))
+		.then(() => {
+			req.flash('success', '建立成功!')
+			res.redirect('/todos')
+		})
 		.catch((err) => console.log(err))
 })
 
@@ -48,7 +60,7 @@ app.get('/todos/:id', (req, res) => {
 		attributes: ['id', 'name', 'isComplete'],
 		raw: true
 	})
-		.then((todo) => res.render('todo', { todo }))
+		.then((todo) => res.render('todo', { todo, message: req.flash('success') }))
 		.catch((err) => console.log(err))
 })
 
@@ -73,7 +85,10 @@ app.put('/todos/:id', (req, res) => {
 		isComplete: isComplete === 'completed',
 		completeAt: isComplete === 'completed' ? new Date() : null
 	}, { where: { id } })
-		.then(()=> res.redirect(`/todos/${id}`))
+		.then(()=> {
+			req.flash('success', '更新成功!')
+			res.redirect(`/todos/${id}`)
+		})
 		.catch((err) => console.log(err))
 })
 
@@ -81,7 +96,10 @@ app.delete('/todos/:id', (req, res) => {
 	const id = req.params.id
 
 	return Todo.destroy({ where: { id } })
-		.then(() => res.redirect('/todos'))
+		.then(() => {
+			req.flash('success', '刪除成功!')
+			res.redirect('/todos')
+		})
 		.catch((err) => console.log(err))
 })
 
