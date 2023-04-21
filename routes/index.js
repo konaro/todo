34a5fafe517976e2426822ccc8616af54f1e6c30
ShangWei Huang <shangwei.huang@iherb.com>
2023-04-22 03:11:23 +0800

    set eb configurations

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const todos = require('./modules/todos')
 
+router.get('/', (_, res) => res.redirect('/todos'))
 router.use('/todos', todos)
 
 module.exports = router
